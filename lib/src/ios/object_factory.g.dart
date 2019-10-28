cd203b6c9133c6d6a509eab6d1b3570cfaafb5e3
yohom <382146139@qq.com>
2019-10-28 16:55:12 +0800

    roll engine #ed34f18

--- lib/src/ios/object_factory.g.dart
+++ lib/src/ios/object_factory.g.dart
@@ -9,7 +9,7 @@ import 'package:flutter/services.dart';
 import 'package:amap_location_fluttify/src/ios/ios.export.g.dart';
 
 // ignore_for_file: non_constant_identifier_names, camel_case_types, missing_return, unused_import
-class ObjectFactory_iOS {
+class AmapLocationFluttifyFactoryIOS {
   static final _channel = MethodChannel('me.yohom/amap_location_fluttify');
 
   static Future<AMapGeoFenceManager> createAMapGeoFenceManager() async {
