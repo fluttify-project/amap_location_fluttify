e8d8d4fc2999ac2353d3fbf22ebf56d2b9f50716
yohom <382146139@qq.com>
2019-12-04 09:37:25 +0800

    enhance: 单次和连续定位单独实现，并返回Future和Stream

--- lib/src/dart/amap_location.dart
+++ lib/src/dart/amap_location.dart
@@ -8,7 +8,13 @@ import 'package:flutter/cupertino.dart';
 import 'enums.dart';
 import 'models.dart';
 
-typedef void _OnLocationChanged(Location location);
+typedef void _OnAndroidLocationChanged(
+  com_amap_api_location_AMapLocation location,
+);
+typedef void _OnIOSLocationChanged(
+  CLLocation location,
+  AMapLocationReGeocode reGeocode,
+);
 typedef void _OnRequireAlwaysAuth(CLLocationManager manager);
 
 /// 释放原生对象Mixin
@@ -25,20 +31,111 @@ class AmapLocation {
   static com_amap_api_location_AMapLocationClient _androidClient;
   static AMapLocationManager _iosClient;
 
+  static StreamController<Location> _locationController;
+
   static final _iosLocationDelegate = _IOSLocationDelegate();
   static final _androidLocationDelegate = _AndroidLocationDelegate();
 
-  /// 获取定位信息
+  /// 单次获取定位信息
   ///
-  /// 选择定位模式[mode], 设置定位同时是否需要返回地址描述[needAddress], 设置定位请求超时时间，默认为30秒[timeout]
-  /// 设置是否开启定位缓存机制[cacheEnable].
-  static void startLocation({
+  /// 选择定位模式[mode], 设置定位同时是否需要返回地址描述[needAddress], 设置定位请求超时时间，默认为30秒[timeout].
+  static Future<Location> fetchLocation({
     LocationAccuracy mode = LocationAccuracy.Low,
     bool needAddress,
     int timeout,
-    bool once = false,
-    @required _OnLocationChanged locationChanged,
   }) async {
+    final completer = Completer<Location>();
+    return platform(
+      android: (pool) async {
+        // 获取上下文, 这里获取的是Application
+        final context = await getandroid_app_Application();
+
+        // 创建定位客户端
+        _androidClient ??=
+            await createcom_amap_api_location_AMapLocationClient__android_content_Context(
+                context);
+
+        // 设置回调
+        await _androidClient.setLocationListener(
+          _androidLocationDelegate
+            .._onLocationChanged =
+                (location) => completer.complete(Location.android(location)),
+        );
+
+        // 创建选项
+        final options =
+            await createcom_amap_api_location_AMapLocationClientOption__();
+        // 设置单次定位
+        await options.setOnceLocation(true);
+        // 设置定位模式
+        if (mode != null)
+          switch (mode) {
+            // 高精度定位模式：会同时使用网络定位和GPS定位，优先返回最高精度的定位结果，以及对应的地址描述信息。
+            case LocationAccuracy.High:
+              await options.setLocationMode(
+                  com_amap_api_location_AMapLocationClientOption_AMapLocationMode
+                      .Hight_Accuracy);
+              break;
+            // 低功耗定位模式：不会使用GPS和其他传感器，只会使用网络定位（Wi-Fi和基站定位）；
+            case LocationAccuracy.Low:
+              await options.setLocationMode(
+                  com_amap_api_location_AMapLocationClientOption_AMapLocationMode
+                      .Battery_Saving);
+              break;
+          }
+        // 是否返回地址描述
+        if (needAddress != null) await options.setNeedAddress(needAddress);
+        // 设置定位请求超时时间，默认为30秒。
+        if (timeout != null) await options.setHttpTimeOut(timeout);
+
+        // 设置选项
+        await _androidClient.setLocationOption(options);
+
+        // 开始定位
+        await _androidClient.startLocation();
+
+        return completer.future;
+      },
+      ios: (pool) async {
+        _iosClient ??= await createAMapLocationManager();
+
+        // 设置定位模式
+        if (mode != null)
+          switch (mode) {
+            // 高精度定位模式：会同时使用网络定位和GPS定位，优先返回最高精度的定位结果，以及对应的地址描述信息。
+            case LocationAccuracy.High:
+              await _iosClient.set_desiredAccuracy(10);
+              break;
+            // 低功耗定位模式：不会使用GPS和其他传感器，只会使用网络定位（Wi-Fi和基站定位）；
+            case LocationAccuracy.Low:
+              await _iosClient.set_desiredAccuracy(100);
+              break;
+          }
+        // 设置定位请求超时时间，默认为30秒。
+        if (timeout != null) await _iosClient.set_locationTimeout(timeout);
+
+        await _iosClient.requestLocationWithReGeocodeCompletionBlock(
+          needAddress ?? true,
+          (location, regeocode, error) {
+            completer.complete(Location.ios(location, regeocode));
+          },
+        );
+        return completer.future;
+      },
+    );
+  }
+
+  /// 连续获取定位信息
+  ///
+  /// 选择定位模式[mode], 设置定位同时是否需要返回地址描述[needAddress], 设置定位请求超时时间，默认为30秒[timeout]
+  /// 设置是否开启定位缓存机制[cacheEnable].
+  static Stream<Location> listenLocation({
+    LocationAccuracy mode = LocationAccuracy.Low,
+    bool needAddress,
+    int timeout,
+  }) async* {
+    _locationController ??= StreamController<Location>();
+
     if (Platform.isAndroid) {
       // 获取上下文, 这里获取的是Application
       final context = await getandroid_app_Application();
@@ -49,14 +146,16 @@ class AmapLocation {
               context);
 
       // 设置回调
-      await _androidClient.setLocationListener(
-          _androidLocationDelegate.._onLocationChanged = locationChanged);
+      await _androidClient.setLocationListener(_androidLocationDelegate
+        .._onLocationChanged = (location) {
+          _locationController.add(Location.android(location));
+        });
 
       // 创建选项
       final options =
           await createcom_amap_api_location_AMapLocationClientOption__();
-      // 设置单次定位
-      await options.setOnceLocation(once);
+      // 设置连续定位
+      await options.setOnceLocation(false);
       // 设置定位模式
       if (mode != null)
         switch (mode) {
@@ -72,13 +171,6 @@ class AmapLocation {
                 com_amap_api_location_AMapLocationClientOption_AMapLocationMode
                     .Battery_Saving);
             break;
-//          // 仅用设备定位模式：不需要连接网络，只使用GPS进行定位，这种模式下不支持室内环境的定位，
-//          // 需要在室外环境下才可以成功定位。注意，自 v2.9.0 版本之后，仅设备定位模式下支持返回地址描述信息。
-//          case LocationAccuracy.DeviceSensor:
-//            await options.setLocationMode(
-//                com_amap_api_location_AMapLocationClientOption_AMapLocationMode
-//                    .Device_Sensors);
-//            break;
         }
       // 是否返回地址描述
       if (needAddress != null) await options.setNeedAddress(needAddress);
@@ -90,6 +182,8 @@ class AmapLocation {
 
       // 开始定位
       await _androidClient.startLocation();
+
+      yield* _locationController.stream;
     } else if (Platform.isIOS) {
       _iosClient ??= await createAMapLocationManager();
 
@@ -108,28 +202,33 @@ class AmapLocation {
       // 设置定位请求超时时间，默认为30秒。
       if (timeout != null) await _iosClient.set_locationTimeout(timeout);
 
-      if (once) {
-        await _iosClient.requestLocationWithReGeocodeCompletionBlock(
-          needAddress ?? true,
-          (location, regeocode, error) {
-            locationChanged(Location.ios(location, regeocode));
-          },
-        );
-      } else {
-        // 设置回调
-        await _iosClient.set_delegate(
-            _iosLocationDelegate.._onLocationChanged = locationChanged);
-        await _iosClient.set_locatingWithReGeocode(true);
-        await _iosClient.startUpdatingLocation();
-      }
+      // 设置回调
+      await _iosClient.set_delegate(_iosLocationDelegate
+        .._onLocationChanged = (location, regeocode) {
+          _locationController.add(Location.ios(location, regeocode));
+        });
+      await _iosClient.set_locatingWithReGeocode(true);
+      await _iosClient.startUpdatingLocation();
+
+      yield* _locationController.stream;
     }
   }
 
   /// 停止定位
   static Future<void> stopLocation() {
     return platform(
-      android: (pool) => _androidClient.stopLocation(),
-      ios: (pool) => _iosClient.stopUpdatingLocation(),
+      android: (pool) async {
+        _locationController?.close();
+        _locationController = null;
+
+        await _androidClient.stopLocation();
+      },
+      ios: (pool) async {
+        _locationController?.close();
+        _locationController = null;
+
+        await _iosClient.stopUpdatingLocation();
+      },
     );
   }
 
@@ -150,6 +249,9 @@ class AmapLocation {
 
   /// 释放对象, 如果[AmapLocationDisposeMixin]不能满足需求时再使用这个方法
   static void dispose() {
+    _locationController?.close();
+    _locationController = null;
+
     final isCurrentPlugin = (it) => it.tag == 'amap_location_fluttify';
     kNativeObjectPool
       ..where(isCurrentPlugin).forEach(release)
@@ -165,7 +267,7 @@ class AmapLocation {
 
 class _AndroidLocationDelegate extends java_lang_Object
     with com_amap_api_location_AMapLocationListener {
-  _OnLocationChanged _onLocationChanged;
+  _OnAndroidLocationChanged _onLocationChanged;
 
   @override
   Future<void> onLocationChanged(
@@ -173,13 +275,13 @@ class _AndroidLocationDelegate extends java_lang_Object
   ) async {
     super.onLocationChanged(var1);
     if (_onLocationChanged != null) {
-      _onLocationChanged(Location.android(var1));
+      _onLocationChanged(var1);
     }
   }
 }
 
 class _IOSLocationDelegate extends NSObject with AMapLocationManagerDelegate {
-  _OnLocationChanged _onLocationChanged;
+  _OnIOSLocationChanged _onLocationChanged;
   _OnRequireAlwaysAuth _onRequireAlwaysAuth;
 
   @override
@@ -194,7 +296,7 @@ class _IOSLocationDelegate extends NSObject with AMapLocationManagerDelegate {
       reGeocode,
     );
     if (_onLocationChanged != null) {
-      _onLocationChanged(Location.ios(location, reGeocode));
+      _onLocationChanged(location, reGeocode);
     }
   }
 
